612b530d2172d118aca114712f14877a03d3cd0d
ivanalazarev <ivanaslazarevic@gmail.com>
2024-06-02 20:37:14 +0200

    Add files via upload

---
+++ edunovawp5 skripta vjezba.sql
@@ -0,0 +1,27 @@
+--use edunovawp5;
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--brojsati int,
+--izvodiseod datetime,
+--vaucer bit
+--);
+
+
+--create table grupe(
+--sifra int,
+--naziv varchar(20),
+--brojslobodnihmjesta int,
+--datumpocetka datetime,
+--smjer int,
+--predavac varchar(50)
+--);
+
+--create table clanovi(
+--grupa int,
+--polaznik int
+--);
+
+
+
